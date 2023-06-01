49a2d0937a3de0f24cae6ca89e91f9ac01da9856
samal36 <sjsamal36@gmail.com>
2023-06-01 02:51:50 -0700

    created the pages and all the wire framing, jus needs to cleanup

---
+++ home_script.js
@@ -0,0 +1,109 @@
+// var day = 'monday';
+// switch (day) {
+//     case 'monday':
+//         console.log('today is monday');
+//         break;
+//     case 'tuesday':
+//         console.log('today is tuesday');
+//         break;
+//     case 'wednesday':
+//         console.log('today is wednesday')
+//     default:
+//         console.log('there is no such day');
+// }
+
+// var marks = 85;
+
+// switch (true) {
+//     case (marks >= 90):
+//         console.log("grade A");
+//         break;
+//     case (marks >= 80):
+//         console.log("grade B");
+//         break;
+//     case (marks >= 70):
+//         console.log("grade c");
+//         break;
+//     case (69 >= marks >= 45):
+//         console.log("grade D");
+//         break;
+//     default:
+//         console.log('you are fail')
+// }
+
+for (i = 0; i < 10; i++) {
+    console.log(i)
+}
+
+// i = 10;
+// while (i > 4) {
+//     console.log('i');
+//     i--;
+// }
+
+// var i = 20;
+// while (i >= 10) {
+//     console.log('this is ', i);
+//     i--;
+// }
+
+// for (i = 1; i < 5; i++) {
+//     for (j = 1; j < 5; j++) {
+//         console.log('the week is' + i + 'and the days is ' + j);
+//     }
+// }
+
+// function addtwonumbers(a, b) {
+//     var c = a + b;
+//     console.log('the value is ' + c);
+// }
+// addtwonumbers(3, 5 )
+
+// function wordfinder(arr) {
+//     for (i = 0; i < arr.length; i++) {
+//         if (arr[i] == 'monday') {
+//             console.log('its is monday in ' + (i + 1) + ' word');
+//         }
+//     }
+// }
+// var dates = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday'];
+// wordfinder(dates)
+
+// var myProfile = {
+//     name: 'samal maleesha',
+//     age: 27,
+//     company: 'diamonds forever'
+
+// }
+// console.log(myProfile.age);
+
+// function arrayBuilder(one, two, three) {
+//     var arr = [];
+//     arr.push(one);
+//     arr.push(two);
+//     arr.push(three);
+//     return arr;
+// }
+// var rr = arrayBuilder(1, 2, 3)
+
+//  
+
+// var clothes = [];
+// clothes.push('shirts');
+// clothes.push('denims');
+// clothes.push('throsers');
+// clothes.push('jacket');
+// clothes.push('caps');
+
+// console.log(clothes);
+// clothes.pop();
+
+// console.log(clothes)
+// clothes.push('underwear')
+// console.log(clothes)
+
+// var favCar = {};
+// favCar.color = 'red'
+// favCar.covertible = true;
+// console.log(favCar)
+
